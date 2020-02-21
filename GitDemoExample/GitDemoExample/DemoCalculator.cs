c9380dd1106eafa1fb1f8549ac861ac7d226d076
Mukund@Codeonnet <39686327+mukundkumar83@users.noreply.github.com>
2020-02-21 15:08:51 +0530

    Added new DemoCalculator and DemoCaluclatorTest class.

---
+++ GitDemoExample/GitDemoExample/DemoCalculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GitDemoExample
+{
+    public class DemoCalculator
+    {
+        public int Sum(int a ,int b)
+        {
+            if(a!=0 && b!=0)
+            {
+                return a + b;
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ GitDemoExample/GitDemoExampleTests/CalculatorTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GitDemoExample;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitDemoExample.Tests
+{
+    [TestClass()]
+    public class CalculatorTests
+    {
+        [TestMethod()]
+        public void SumTest()
+        {
+            int a = 10;
+            int b = 20;
+            DemoCalculator demoCalculator = new DemoCalculator();
+            int result =demoCalculator.Sum(a, b);
+            Assert.AreEqual(30,result);
+        }
+    }
+}
